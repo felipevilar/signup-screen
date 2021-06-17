754be4a6be43c4b502bf83a10494890a81f11a51
felipevilar <felipvilar@gmail.com>
2021-06-16 20:01:26 -0400

    screen signup

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,6 @@ import FormSignup from "./components/FormSignup.vue";
 import LoginText from "./components/LoginText.vue";
 import BaseButton from "./components/BaseButton.vue";
 
-
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
--- src/components/FormSignup.vue
+++ src/components/FormSignup.vue
@@ -4,15 +4,33 @@
 
 <template>
   <form class="form-signup drop-shadow" @submit.prevent="submit">
-    <BaseInput v-model="user.name" type="text" placeholder="Name" />
-    <BaseInput v-model="user.lastname" type="text" placeholder="Last Name" />
-    <BaseInput v-model="email" type="text" placeholder="Email" />
-    <BaseInput v-model="user.password" type="password" placeholder="Password" />
-
-    <!--       <button type="submit" class="btn form-button inner-shadow">
-        {{ msg }}
-      </button> -->
-
+    <BaseInput
+      v-model="user.name"
+      type="text"
+      placeholder="Name"
+      :class="{ 'form-input-error': !validName }"
+      @blur="valide()"
+    />
+    <BaseInput
+      v-model="user.lastname"
+      type="text"
+      placeholder="Last Name"
+      :class="{ 'form-input-error': !validLastName }"
+      @blur="valide()"
+    />
+    <BaseInput
+      v-model="user.email"
+      type="text"
+      placeholder="Email"
+      :class="{ 'form-input-error': !validEmail }"
+    />
+    <BaseInput
+      v-model="user.password"
+      type="password"
+      placeholder="Password"
+      :class="{ 'form-input-error': !validPassword }"
+      @blur="valide()"
+    />
     <BaseButton type="submit" class="form-button inner-shadow">{{
       msg
     }}</BaseButton>
@@ -23,24 +41,66 @@
       &nbsp<a href="#">Terms and Services</a>
     </div>
   </form>
+  <div>
+    <p v-for="error of v$.$errors" :key="error.$uid">
+      <strong>{{ error.$validator }}</strong>
+      <small> on property</small>
+      <strong>{{ error.$property }}</strong>
+      <small> says:</small>
+      <strong>{{ error.$message }}</strong>
+    </p>
+  </div>
 </template>
 
 <script>
 import BaseInput from "./BaseInput.vue";
 import BaseButton from "./BaseButton.vue";
+import useVuelidate from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
 
 export default {
   components: { BaseInput, BaseButton },
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data: function () {
     return {
+      validEmail: true,
+      validName: true,
+      validLastName: true,
+      validPassword: true,
+      submited: false,
       msg: "CLAIM YOUR FREE TRIAL",
-      user: {},
-      email: null,
+      user: {
+        name: "",
+        lastName: "",
+        email: "",
+        password: "",
+      },
+    };
+  },
+  validations() {
+    return {
+      user: {
+        name: { required }, // Matches this.contact.email
+        lastName: { required, email }, // Matches this.contact.email
+        email: { required, email }, // Matches this.contact.email
+        password: { required, email }, // Matches this.contact.email
+      },
     };
   },
   methods: {
+    valide() {
+      if (this.submited) {
+        this.validName = !this.v$.user.name.$invalid;
+        this.validLastName = !this.v$.user.lastName.$invalid;
+        this.validPassword = !this.v$.user.password.$invalid;
+        this.validEmail = !this.v$.user.email.$invalid;
+      }
+    },
     submit() {
-      console.log(this.user.name, this.user.lastname);
+      this.submited = true;
+      this.valide();
     },
   },
 };
